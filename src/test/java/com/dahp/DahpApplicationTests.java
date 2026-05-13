d8cb891be4a78a117b9834743ba221d31b208e0f
luckyisjelly <assignment89@naver.com>
2026-05-13 09:52:44 +0900

    Initial commit: Spring Boot 4 starter for DAHP
    - Spring Boot 4.0.6 with Java 21
    - Dependencies: data-jpa, security, validation, webmvc, lombok, devtools, postgresql

---
+++ src/test/java/com/dahp/DahpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DahpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
